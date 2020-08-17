8f3057120397128238b48c2ef10b68efa324c12f
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-08-17 16:49:48 +0100

    location tracker API

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes.js');
+const tracksRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(tracksRoutes);
+
+const api_key = process.env.DB_PASS
+const mongoUri = `mongodb+srv://admin-fabricio:${api_key}@cluster0.i9ftq.mongodb.net/<dbname>?retryWrites=true&w=majority`
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('ERROR', (err) => {
+    console.log('Connected to mongo instance', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
